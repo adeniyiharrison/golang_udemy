f780e4320ebe62119176e9de63ead4688e2ec4f2
adeniyiharrison <adeniyi@shipt.com>
2021-03-06 15:48:16 -0800

    3/6 3:50PM // exercise ninja 10 complete

--- 11_channels/exercise/exercise.go
+++ 11_channels/exercise/exercise.go
@@ -1,4 +1,146 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+)
+
 func main() {
+	// p1()
+	// p2()
+	// p3()
+	// p4()
+	// p5()
+	p6()
+}
+
+func p1() {
+	c := make(chan int)
+	c2 := make(chan int, 1)
+
+	go func() {
+		c <- 1
+	}()
+
+	c2 <- 2
+
+	fmt.Println("method #1:", <-c)
+	fmt.Println("method #2:", <-c2)
+}
+
+func p2() {
+	cs := make(chan int)
+	go func() {
+		cs <- 1
+	}()
+	fmt.Println(<-cs)
+	fmt.Printf("%T\n", cs)
+}
+
+func p3() {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for value := range c {
+		fmt.Println(value)
+	}
+
+	// for {
+	// 	select {
+	// 	case value, ok := <-c:
+	// 		if ok {
+	// 			fmt.Println(value)
+	// 		} else {
+	// 			return
+	// 		}
+	// 	}
+	// }
+}
+
+func p4() {
+	q := make(chan int)
+	gen(q)
+	recieve(q)
+	fmt.Println("exit func")
+}
+
+func gen(c chan<- int) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+}
+
+func recieve(c <-chan int) {
+	for {
+		select {
+		case value, ok := <-c:
+			if ok {
+				fmt.Println(value)
+			} else {
+				fmt.Println("exit select")
+				return
+			}
+		}
+	}
+}
+
+func p5() {
+	channel := make(chan int)
+
+	go func(c chan<- int) {
+		for i := 0; i < 100; i++ {
+			c <- rand.Intn(100)
+		}
+		close(c)
+	}(channel)
+
+	func(c <-chan int) {
+		for {
+			select {
+			case value, ok := <-c:
+				if ok {
+					fmt.Println(value)
+				} else {
+					return
+				}
+			}
+		}
+	}(channel)
+}
+
+func p6() {
+
+	threads := 10
+	var waiter sync.WaitGroup
+
+	channel := make(chan int)
+	go func() {
+		for i := 0; i < threads; i++ {
+			fmt.Println("Number of Routines:", runtime.NumGoroutine())
+			waiter.Add(1)
+			go func() {
+				for j := 0; j < 10; j++ {
+					channel <- rand.Intn(100)
+				}
+				waiter.Done()
+			}()
+		}
+		waiter.Wait()
+		close(channel)
+	}()
+
+	for v := range channel {
+		fmt.Println(v)
+	}
 }
--- 11_channels/exercise/exercise.md
+++ 11_channels/exercise/exercise.md
@@ -1,3 +1,60 @@
 # Ninja Level 10
 
-1. 
+1. Get the following code to function
+```
+    func p1(){
+        c := make(chan int)
+        c <- 42
+        fmt.Println(<-c)
+    }
+```
+
+2. Get the following code to function
+```
+    func p2(){
+        cs := make(chan<- int)
+        go func(){
+            cs<- 1
+        }()
+        fmt.Println(<-cs)
+        ftm.Printf("%T\n", cs)
+    }
+```
+
+3. Fix this
+```
+    func p3() {
+        c := make(chan int)
+
+        go func() {
+            for i := 0; i < 10; i++ {
+                c <- i
+            }
+        }()
+
+        for {
+            fmt.Println(<-c)
+        }
+    }
+```
+
+4. Pull values down using select statement
+```
+    func p4(){
+        q := make(chan int)
+        c := gen(q)
+        recieve(c, q)
+        fmt.Println("exit")
+    }
+
+    func gen(c <-chan int) <-chan int{
+        for i := 0; i < 10; i++ {
+            c <- i
+        }
+
+        return c
+    }
+```
+5. Write a program that writes 100 numbers to a channel then print them
+6. Write a program that launches 10 goroutines that add 10 numbers to a channel
+    * Pull off from the channel and print them
